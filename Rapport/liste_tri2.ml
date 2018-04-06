bd56b90950307357f26a82ff3f4b03585b7bc694
Clement Defretiere <clement.def.7@gmail.com>
2018-04-06 08:31:13 +0200

    Added partitionne/fusionne

--- Rapport/liste_tri2.ml
+++ Rapport/liste_tri2.ml
@@ -3,6 +3,8 @@
 Hasard.init_random ();;
 Random.self_init ();;
 
+(* Tri par creation du maximum *)
+
 let rec selectionne_max comp l = match l with
 	| [] -> failwith "Cannot find a max in an empty list"
 	| [x] -> x
@@ -30,4 +32,30 @@ let rec tri_creation_max comp l =
 			ajoute_fin max (tri_creation_max comp temp_list)
 ;;
 
- tri_creation_max (>) (Hasard.random_list 10 100);;
+tri_creation_max (>) (Hasard.random_list 10 100);;
+
+ (* Tri par partition-fusion *)
+
+let partitionne l =
+	let rec aux p l = match l with
+	| [] -> []
+	| [x] -> if p then [x] else []
+	| x::(y::z) -> if p then x::(aux true z) else y::(aux false z)
+	in
+	((aux true l), (aux false l))
+;;
+
+(* let rec fusionne comp l1 l2 =
+	let len1 = List.length l1 and len2 = List.length l2 in
+		if len1 = 0 then l2 else if len2 = 0 then l1 
+	let f1 = List.nth l1 0 and f2 = List.nth l2 0
+	and r1 = (List.tl l1) and r2 = (List.tl l2) in
+		if comp f1 f2 then f1::(f2::(fusionne comp r1 r2)) else f2::(f1::(fusionne comp r1 r2))
+;; *)
+
+let rec fusionne comp l1 l2 = match l1, l2 with
+	| a::b, x::y -> if comp a x then a::(fusionne comp b l2) else x::(fusionne comp l1 y)
+	| _, _ -> l1@l2
+;;
+
+fusionne (<) [1; 4; 6; 7] [2; 5; 8];;
