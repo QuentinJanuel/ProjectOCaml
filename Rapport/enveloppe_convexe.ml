58afbe79c62d78fb77430715910358fa78863e87
Clement Defretiere <clement.def.7@gmail.com>
2018-04-09 17:39:54 +0200

    Added "calcul de pa, pb" from part 2

--- Rapport/enveloppe_convexe.ml
+++ Rapport/enveloppe_convexe.ml
@@ -1,8 +1,14 @@
 #load "graphics.cma";;
+#load "point.cmo";;
+#load "list_tri.cmo";;
 
 open Graphics;;
+open Point;;
+open List_tri;;
 
-open_graph "";;
+let min_point p1 p2 = if p1.abs < p2.abs then true else false;;
 
-let rec loop () = loop () in
-	loop ();;
+let max_point p1 p2 = if p1.abs > p2.abs then true else false;;
+
+let points_depart l = (min_list min_point l), (min_list max_point l);;
+
